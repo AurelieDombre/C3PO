ad365b42d448c9a97a7a9d02d830116c5697956f
AurelieDombre <au.dombre@gmail.com>
2026-05-28 09:13:41 +0200

    version fonctionnelle de l'installation à l'execution des recherches

--- backend/start_backend.py
+++ backend/start_backend.py
@@ -1,8 +1,10 @@
+from api.main import app
 import uvicorn
 
-uvicorn.run(
-    "api.main:app",
-    host="localhost",
-    port=8000,
-    reload=False
-)
+if __name__ == "__main__":
+
+    uvicorn.run(
+        app,
+        host="127.0.0.1",
+        port=8000
+    )
